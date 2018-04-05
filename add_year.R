dd51f1df93d3c31968337f9e3a22471bdedfd5c3
Fangzhu <zhouzhou602@gmail.com>
2018-04-05 09:36:09 -0700

    adding year data

---
+++ add_year.R
@@ -0,0 +1,38 @@
+
+year.vector <- rep(1986, length = nrow(data.1986))
+data.1986$year <- year.vector
+
+year.vector <- rep(1987, length = nrow(data.1986))
+data.1987$year <- year.vector
+
+year.vector <- rep(1988, length = nrow(data.1986))
+data.1988$year <- year.vector
+
+year.vector <- rep(1989, length = nrow(data.1986))
+data.1989$year <- year.vector
+
+year.vector <- rep(1990, length = nrow(data.1986))
+data.1990$year <- year.vector
+
+year.vector <- rep(1991, length = nrow(data.1986))
+data.1991$year <- year.vector
+
+year.vector <- rep(1992, length = nrow(data.1986))
+data.1992$year <- year.vector
+
+year.vector <- rep(1993, length = nrow(data.1986))
+data.1993$year <- year.vector
+
+year.vector <- rep(1994, length = nrow(data.1986))
+data.1994$year <- year.vector
+
+year.vector <- rep(1995, length = nrow(data.1986))
+data.1995$year <- year.vector
+
+year.vector <- rep(1996, length = nrow(data.1986))
+data.1996$year <- year.vector
+
+year.vector <- rep(1997, length = nrow(data.1986))
+data.1997$year <- year.vector
+
+
