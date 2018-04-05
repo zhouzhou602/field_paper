cddceacd7d3d34a1c896268129f39650bed80cbd
Fangzhu <zhouzhou602@gmail.com>
2018-04-05 15:56:26 -0700

    I don't wanna this

--- add_year.R
+++ add_year.R
@@ -1,3 +1,12 @@
+seq_yr <- seq(1986,1997)
+files <- list(data.1986,data.1987,data.1988,data.1989,data.1990,data.1991,data.1992,data.1993
+              ,data.1994,data.1995,data.1996,data.1997)
+for (i in 1:length(seq_yr)){
+  Year <- rep(i+1985,length = nrow(data.1986))
+  a <- cbind(files[[i]], Year)
+  assign(paste0("data.",1985+i),a)
+}
+
 
 year.vector <- rep(1986, length = nrow(data.1986))
 data.1986$year <- year.vector
