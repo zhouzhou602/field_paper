20cd2014612946280657674ada78b0d0e13da340
Fangzhu <zhouzhou602@gmail.com>
2018-04-08 16:45:46 -0700

    add loop to merge data

--- merge_updated.R
+++ merge_updated.R
@@ -2,6 +2,8 @@ setwd("/Users/Fangzhu/Documents/field_paper/8515")
 source(data8515_updated.R)
 
 ##Firstly, only keep the data with complete information.
+firstmrg <- subset(data.1985,hd_num_mrg<2)
+data.1985 <- firstmrg
 files <- list(data.1985,data.1986,data.1987,data.1988,data.1989,data.1990,data.1991,data.1992,data.1993
               ,data.1994,data.1995,data.1996)
 for(i in 1:length(files)){
@@ -25,11 +27,18 @@ for(i in 1:length(files2)){
   assign(paste0("ID",2*i+1995),ID)
 }
 
+red_files <- list(data.1985,data.1986,data.1987,data.1988,data.1989,data.1990,data.1991,data.1992,data.1993
+              ,data.1994,data.1995,data.1996)
+
+red_files2 <- list(data.1997,data.1999,data.2001,data.2003,data.2005
+               , data.2007, data.2009, data.2011, data.2013, data.2015)
+#loop
 newfile <- data.1985
-for(i in 2:length(files)){
-  newfile <- merge(newfile,files[[i]],all=TRUE)
+for(i in 2:length(red_files)){
+  newfile <- merge(newfile2,red_files[[i]],all=TRUE)
 }
-for(i in 1:length(files2)){
+
+for(i in 1:length(red_files2)){
   newfile <- merge(newfile,files2[[i]],all=TRUE)
 }
 
