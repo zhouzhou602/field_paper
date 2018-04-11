d129ec687e7f568dad8e42d8de005c03478dde08
Fangzhu <zhouzhou602@gmail.com>
2018-04-10 23:09:19 -0700

    add freq for income ratio

--- merge_updated.R
+++ merge_updated.R
@@ -1,3 +1,5 @@
+library(strata)
+library(ggplot2)
 setwd("/Users/Fangzhu/Documents/field_paper/8515")
 source(data8515_updated.R)
 
@@ -21,6 +23,7 @@ files2 <- list(data.1997,data.1999,data.2001,data.2003,data.2005
 
 for(i in 1:length(files2)){
   dt <- files2[[i]]
+  dt <- subset(dt,hd_income<9999999 && wf_income<9999999)
   dt <- na.omit(dt)
   ID <- dt$ID
   assign(paste0("data.",2*i+1995),dt)
@@ -35,15 +38,41 @@ red_files2 <- list(data.1997,data.1999,data.2001,data.2003,data.2005
 #loop
 newfile <- data.1985
 for(i in 2:length(red_files)){
-  newfile <- merge(newfile2,red_files[[i]],all=TRUE)
+  newfile <- merge(newfile,red_files[[i]],all=TRUE)
 }
 
 for(i in 1:length(red_files2)){
   newfile <- merge(newfile,files2[[i]],all=TRUE)
 }
 
+##Sort by ID and year
 sort_newfile <- newfile[order(newfile$ID,newfile$Year),]
 
+##Hours of housework should <= 99
+##Age should < 99
+#sex of 1985 had == 1 for male
+#Marital status = 1 -- married; 2 -- never married; 3 -- widowed; 4 --divorced; 5 -- separated
+#Employment status: 1 -- working now; 2 -- only temporarily laid off, sick leave or maternity leave; 3 -- looking for work, unemployed; 4 -- retired; 5 -- disabled; 6 -- keeping house; 7 -- student; 8 -- other
+#Health: 1-- excellent --> 5 -- poor ; 8 -- DK; 9 -- NA
+#Wtr_wf in FU: 1 -- head is male, wife in FU 2 -- head is male, wife in FU 3 -- head is female
+
+dt <- subset(sort_newfile,hd_wage < 9999998) ##9,999,999 means NA or refused and 9,999,998 means DK
+dt <- subset(dt,wf_wage < 9999998)
+dt <- subset(dt,hd_housework_hrs<99 && wf_housework_hrs<99)
+dt <- subset(dt,head_sex<2) 
+dt <- subset(dt,head_marital_status != 3) #extract widowed head
+dt <- subset(dt,hd_health<9 && wf_health<9) #extract NAs in health_status
+dt <- subset(dt,wtr_wf<3) ##eliminate the case where head is female
+dt_with_wf <- subset(dt,wtr_wf == 1)
+dt_female <- subset(sort_newfile,head_sex > 1)
+dt_with_wf$wage_ratio <- dt_with_wf$wf_adj_income/(dt_with_wf$hd_adj_income+dt_with_wf$hd_adj_income)
+dt_both_work <- subset(dt_with_wf,hd_wage>0 & wf_wage>0)
 
 
+breaks = seq(0,1,by=0.1)
+cut = cut(dt_both_work$wage_ratio,breaks,right=FALSE)
+freq <- table(cut)
+cbind(freq)
+plot(cut)
 
+##check the distribution for working hours ratio, variables_list_4_10.csv has been updated.
